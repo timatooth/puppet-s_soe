3efb7264c3ec211b0904b4f97993d55cc6cba0cb
Tim Sullivan <tsullivan@timatooth.com>
2016-08-25 23:31:44 +1200

    Using create resources with hiera params

--- manifests/users.pp
+++ manifests/users.pp
@@ -1,11 +1,7 @@
-class s_soe::users {
-  accounts::user { 'tim':
-    ensure   => present,
-    groups   => ['admin'],
-    uid      => 4001,
-    gid      => 4001,
-    password => '!!',
-    sshkeys  => ['ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCe5Y+qcWALJVmp9F9IIq2Xj83AQpeZg/02jDaZtZoeDmDbIi4EwkDnEykhJGfgQRz9JClwEoleSpbDj6U9qK8KlPj2yfg8TWDaG0WrD0LXTbELwiXMPvoOeWLD8EouM3WEIhsG8Sf07eRztaSi6OJuQlaaD79V4GSZ+NnljgJ1+31QlTUAJKX+UdR3w9bo/HW1XJFJvhGaW1Y9SyE98R8M4bstoqv0DvtmUmsf/cHgLyDMWPCXBty4uoPMAavgs5nvaUPhCPBeobENIi8rOHjzNtdJtXShFicCccEDBg7Nj6u+eG4fNlvbNVw+h3/CgYAnTUEGyejrFKnzbhmNguY1'],
+class s_soe::users ($users = undef) {
+  if !$users {
+    warning('No users found from hiera for s_soe::users')
   }
+  create_resources(accounts::user, $users)
 }
 
